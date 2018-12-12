a355b36bb189bab09e24734c4059bfad921ea3dd
karabash <whyewre@gmail.com>
2018-12-12 19:20:28 +0100

    added comments

--- Assignment3/src/playerAttributes/PlayerAttributes.java
+++ Assignment3/src/playerAttributes/PlayerAttributes.java
@@ -1,10 +1,14 @@
 package playerAttributes;
 
+// This interface has to be implement by players 
+// because every player has a name
 public interface PlayerAttributes {
+
+	// implicitly abstract method (you can think as public)
+	//takes one arguments as String
 	void setName(String  playerName);
 
+	// implicitly abstract method (you can think as public)
+	//this abstract method returns String
 	String getName();
-
-
-
 }
