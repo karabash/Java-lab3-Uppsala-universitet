5425f49772d514dd9d5ed2768ffd272473c537b4
karabash <whyewre@gmail.com>
2018-12-12 16:44:43 +0100

    comments are added

--- Assignment3/src/Table/PlayerGreeting.java
+++ Assignment3/src/Table/PlayerGreeting.java
@@ -1,6 +1,11 @@
 package Table;
 @FunctionalInterface
+//Every game has a greeting so
+//a game has to implement this interface
 public interface PlayerGreeting {
+	// this abstract method returns player name
+	// and takes an argument as String to
+	// to say welcome with user with his/her name
 	String greatingUser(String  playerName);
 
 }
