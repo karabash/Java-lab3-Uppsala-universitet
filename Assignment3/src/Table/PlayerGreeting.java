280effa433eb9a87ef81c30d14d6e6c2db1c185c
karabash <whyewre@gmail.com>
2018-12-12 19:22:40 +0100

    i have removed argument

--- Assignment3/src/Table/PlayerGreeting.java
+++ Assignment3/src/Table/PlayerGreeting.java
@@ -4,8 +4,8 @@ package Table;
 //a game has to implement this interface
 public interface PlayerGreeting {
 	// this abstract method returns player name
-	// and takes an argument as String to
-	// to say welcome with user with his/her name
+	// and takes no argument.
+	// Purpuse say welcome to user with with his/her name
 	String greatingUser();
 
 }
