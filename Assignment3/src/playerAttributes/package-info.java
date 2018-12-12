65aae72156d2975879e623f8a9efe93027a6f86f
karabash <whyewre@gmail.com>
2018-12-12 16:37:44 +0100

    Comments are added

--- Assignment3/src/playerAttributes/package-info.java
+++ Assignment3/src/playerAttributes/package-info.java
@@ -1,10 +1,14 @@
-/**
- * 
- */
-/**
- * @author #Karabash
- * This package contains player attributes
- * and has to be implemented by player - players
- * class - classes
- */
 package playerAttributes;
+
+// This interface has to be implement by players 
+// because every player has a name
+public interface PlayerAttributes {
+
+	// implicitly abstract method (you can think as public)
+	//takes one arguments as String
+	void setName(String  playerName);
+
+	// implicitly abstract method (you can think as public)
+	//this abstract method returns String
+	String getName();
+}
